7dd289fac0de9588806b489506d711b817b6ee7f
jaimelias <escribeme@jaimelias.com>
2022-11-26 11:17:12 -0500

    fixed validation

--- public/class-dynamicaviation-price-table.php
+++ public/class-dynamicaviation-price-table.php
@@ -19,12 +19,6 @@ class Dynamic_Aviation_Price_Table {
 
 	public function table($iata = '')
 	{
-
-		if(is_admin())
-		{
-			return '';
-		}
-
 		$output = '';
 		$airport_array = $this->utilities->airport_data();
 		$is_aircraft_page = is_singular('aircrafts');
