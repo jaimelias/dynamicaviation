7f0aab6efb7ecc04d3c612db2bc0d1a3e03e335c
Jaime Castillo <escribeme@jaimelias.com>
2024-02-10 18:41:51 -0500

    fixed phone inputs styles

--- public/class-dynamicaviation-estimate-table.php
+++ public/class-dynamicaviation-estimate-table.php
@@ -436,16 +436,17 @@ class Dynamic_Aviation_Estimate_Table {
                     <div class="pure-g gutters">
                         <div class="pure-u-1 pure-u-md-1-2">
                             <div class="bottom-20">
-                                <label for="countryCallingCode"><?php echo (esc_html__('Country Calling Code', 'dynamicaviation')); ?></label>
-                                <select name="country_calling_code" class="countryCallingCode"><option>--</option></select>
-                            </div>
-                        </div>
-                        <div class="pure-u-1 pure-u-md-1-2">
-                            <div class="bottom-20">
                                 <label for="phone"><?php echo (esc_html__('Phone', 'dynamicaviation')); ?></label>
-                                <input type="number" name="phone" />
+                                <div class="pure-g">
+                                    <div class="pure-u-1-2">
+                                            <select name="country_calling_code" class="countryCallingCode"><option>--</option></select>
+                                    </div>
+                                    <div class="pure-u-1-2">
+                                         <input type="number" name="phone" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>								
+                        </div>							
                     </div>
 
                                     
