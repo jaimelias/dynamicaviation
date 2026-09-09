43daff5702efffa599c97e748c9fd29a6e82c879
Jaime Castillo <escribeme@jaimelias.com>
2026-09-09 18:07:06 -0500

    upgraded dy–core folder order - 1.0.51

--- dynamicaviation.php
+++ dynamicaviation.php
@@ -16,7 +16,7 @@
  * Plugin Name: Dynamic Aviation
  * Plugin URI: https://www.jaimelias.com
  * Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version: 1.0.50
+ * Version: 1.0.51
  * Author: Jaimelías
  * Author URI: https://jaimelias.com
  * License: GPL-2.0+
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define('DYNAMICAVIATION_VERSION', '1.0.50');
+define('DYNAMICAVIATION_VERSION', '1.0.51');
 
 /**
  * The code that runs during plugin activation.
