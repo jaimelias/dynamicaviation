635e88ea47fb9ead500c6f49834f6486cacfa2db
Jaime Castillo <escribeme@jaimelias.com>
2026-09-09 15:47:44 -0500

    added is_local_host

--- dynamicaviation.php
+++ dynamicaviation.php
@@ -8,27 +8,29 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://jaimelias.com
- * @since             1.0.0
- * @package           Dynamic_Aviation
+ * @link https://jaimelias.com
+ * @since 1.0.0
+ * @package Dynamic_Aviation
  *
  * @wordpress-plugin
- * Plugin Name:       Dynamic Aviation
- * Plugin URI:        https://www.jaimelias.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Jaimelías
- * Author URI:        https://jaimelias.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       dynamicaviation
- * Domain Path:       /languages
+ * Plugin Name: Dynamic Aviation
+ * Plugin URI: https://www.jaimelias.com
+ * Description: This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version: 1.0.42
+ * Author: Jaimelías
+ * Author URI: https://jaimelias.com
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: dynamicaviation
+ * Domain Path: /languages
  */
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+define('DYNAMICAVIATION_VERSION', '1.0.42');
 
 /**
  * The code that runs during plugin activation.
