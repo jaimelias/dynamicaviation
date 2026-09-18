e308f32171cbc505c9d818182153ba93ac81c8ad
Jaime Castillo <escribeme@jaimelias.com>
2026-09-18 13:31:32 -0500

    replaced sanitize_email for dy_sanitize_email - 1.1.43

--- public/class-dynamicaviation-wp-json.php
+++ public/class-dynamicaviation-wp-json.php
@@ -75,7 +75,7 @@ class Dynamic_Aviation_WP_JSON {
 
         $unique_tx_id = wp_generate_uuid4();
         if (!dy_tx::create($unique_tx_id, [
-            'email' => sanitize_email($request['email']),
+            'email' => dy_sanitize_email((string) $request['email']),
             'dy_request' => 'estimate_request',
             'dy_id' => $dy_id,
         ])) {
